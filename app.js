21a4933f4959aa6a186bbea17360702cbe6fc406
RossieK <rosi.kirova96@gmail.com>
2021-04-19 19:22:48 +0300

    Add scroll animations

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let controller;
 let slideScene;
+let pageScene;
 
 function animateSlides() {
     //Initiate controller
@@ -8,7 +9,7 @@ function animateSlides() {
     const sliders = document.querySelectorAll('.slide');
     const nav = document.querySelector('.nav-header');
     //Loop over each slide
-    sliders.forEach(slide => {
+    sliders.forEach((slide, index, slides) => {
         const revealImg = slide.querySelector('.reveal-img');
         const img = slide.querySelector('img');
         const revealText = slide.querySelector('.reveal-text');
@@ -18,6 +19,31 @@ function animateSlides() {
         slideTimeline.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
         slideTimeline.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
         slideTimeline.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
+        //Create Scene
+        slideScene = new ScrollMagic.Scene({
+                triggerElement: slide,
+                triggerHook: 0.25,
+                reverse: false
+            })
+            .setTween(slideTimeline)
+            .addIndicators({ colorStart: 'white', colorTrigger: 'white', name: 'slide' })
+            .addTo(controller);
+        //New Animation
+        const pageTimeline = gsap.timeline();
+        let nextSlide = slides.length - 1 === index ? 'end' : slides[index + 1];
+        pageTimeline.fromTo(nextSlide, { y: '0%' }, { y: '50%' });
+        pageTimeline.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
+        pageTimeline.fromTo(nextSlide, { y: '50%' }, { y: '0%' }, '-=0.5');
+        //Create new scene
+        pageScene = new ScrollMagic.Scene({
+                triggerElement: slide,
+                duration: '100%',
+                triggerHook: 0
+            })
+            .addIndicators({ colorStart: 'white', colorTrigger: 'white', name: 'page', indent: 200 })
+            .setPin(slide, { pushFollowers: false })
+            .setTween(pageTimeline)
+            .addTo(controller)
     });
 
 }
